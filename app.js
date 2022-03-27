d8ccd90968e9f9512219619e5a5e9c38f1386f6f
ezracb <ezra_cb@yahoo.com>
2022-03-27 16:20:57 +0700

    master

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 import ChordSheetJS from 'chordsheetjs';
+import http from 'http';
+import fs from 'fs';
+import express from 'express';
+
+fs.readFile('./index.html', function (err, html) {
+       if (err) {
+           throw err; 
+       }       
+       http.createServer(function(request, response) {  
+           response.writeHeader(200, {"Content-Type": "text/html"});  
+           response.write(html);  
+           response.end();  
+       }).listen(8000);
+   });
+
+    const app = express();
+    const port = 3000;
+    app.get('/helloworld', (req, res) => {
+        res.send(regtochordpro())
+    });
+    app.listen(port, () => {
+        console.log(`cli-nodejs-api listening at http://localhost:${port}`)
+    });
 
 
-const chordSheet = `
-       Am         C/G        F          C
-Let it be, let it be, let it be, let it be
-C                G              F  C/E Dm C
-Whisper words of wisdom, let it be`.substring(1);
+function regtochordpro(){
+    const chordSheet = `
+        Am         C/G        F          C
+    Let it be, let it be, let it be, let it be
+    C                G              F  C/E Dm C
+    Whisper words of wisdom, let it be`.substring(1);
 
-const parser = new ChordSheetJS.ChordSheetParser();
-const song = parser.parse(chordSheet);
+    const parser = new ChordSheetJS.ChordSheetParser({ preserveWhitespace: false });
+    const song = parser.parse(chordSheet);
 
 
-const formatter = new ChordSheetJS.ChordProFormatter({ preserveWhitespace: false });
-const disp = formatter.format(song);
+    const formatter = new ChordSheetJS.ChordProFormatter();
+    const disp = formatter.format(song);
+
+    return disp;
+}
+
